e9b877592c4abc4505e5bf42b01078bd0c6fba15
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-05-03 16:48:36 +0900

    Chapter10本の追加登録処理

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use app\Models\Book;
+// use Dotenv\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,26 @@ Route::get('/', function () {
 
 //新しい「本」を追加
 Route::post('/books',function (Request $request) {
-    //
+    //バリデーション all()は入力値を全て連想配列で取得
+    $validator = Validator::make($request->all(), [
+        //複数のバリデーションの設定が可能↓
+        'item_name' => 'required|max:255',
+    ]);
+
+    //バリデーションエラー
+    if ($validator->fails()) {
+        return redirect('/') //ルートへリダイレクト
+            ->withInput() //セッションに値を保存
+            ->withErrors($validator);
+    }
+    //Eloquentモデル
+    $books = new Book;
+    $books -> item_name = $request->item_name;
+    $books -> item_number = '1';
+    $books -> item_amount = '1000';
+    $books -> publiched = '2017-03-07 00:00:00';
+    $books -> save(); //ModelクラスがDB登録のsaveメソッドを持っている。
+    return redirect('/');
 });
 
 //本を削除
