17853bc73cd77af2cb50f84e8d3e25fdcd6f8f85
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-05-04 22:25:15 +0900

    エラー修正

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Book;
+// use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 // use App\Http\Controllers\BooksController;
@@ -21,13 +21,13 @@ use Illuminate\Support\Facades\Validator;
 Route::get('/','App\Http\Controllers\BooksController@index');
 
 //本を削除 削除する本の情報渡したいので引数は$book
-Route::delete('/book/{book}','BooksController@destroy');
+Route::delete('book/{book}','App\Http\Controllers\BooksController@destroy');
 
 //更新画面 削除する本の情報渡したいので引数は$book
-Route::post('/booksedit/{books}','App\Http\Controllers\BooksController@edit');
+Route::post('booksedit/{books}','App\Http\Controllers\BooksController@edit');
 
 //更新処理 更新内容を入力した情報を渡したいので$request
-Route::post('/books/update', 'App\Http\Controllers\BooksController@update');
+Route::post('books/update', 'App\Http\Controllers\BooksController@update');
 
 //新しい「本」を追加 追加ボタンを押すとここにルーティングされる 本の情報を持っているので引数は$request
-Route::post('/books','App\Http\Controllers\BooksController@store');
+Route::post('books','App\Http\Controllers\BooksController@store');
